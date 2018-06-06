07e7040a967f5d81c85a9f6a77f978d5ee9bb9c3
Lachlan Agnew <lachlanagnew@outlook.com>
2018-06-06 17:02:25 +1000

    Set up basics of blog posts

--- client/src/components/post.js
+++ client/src/components/post.js
@@ -10,7 +10,15 @@ class Posts extends Component {
     return (
       <div style={{padding: 50}} >
         <div style={{width: '100%', height: '200px', boxShadow: '6px 10px 31px 0 rgba(61, 70, 80, .3)'}}>
-          Hello
+          <div style={{display: 'inline-block', width: '30%', height: '100%', backgroundImage: 'url(' + this.props.post.image + ')', backgroundRepeat: 'no-repeat'}} />
+          <div style={{display: 'inline-block', width: '60%', height: '100%'}}>
+            <h2>
+              {this.props.post.title}
+            </h2>
+            <div>
+              {this.props.post.preview}
+            </div>
+          </div>
         </div>
       </div>
     )
--- client/src/components/posts.js
+++ client/src/components/posts.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Slider from 'react-slick'
 import Post from './post'
 
+import cloudIcon from '../assets/icons/svg/cloud-computing-1.svg'
+
 const settings = {
   dots: true,
   infinite: true,
@@ -10,18 +12,41 @@ const settings = {
   slidesToScroll: 1
 }
 
+const blogs = [{
+  title: 'MyStudyBuddy',
+  preview: 'case study for myStudyBuddy',
+  image: cloudIcon,
+  body: '',
+  author: '',
+  time: ''
+}, {
+  title: 'Technical Post',
+  preview: '',
+  image: cloudIcon,
+  body: '',
+  author: '',
+  time: ''
+}, {
+  title: 'Why you need a website.',
+  preview: 'why does your company need a website upgrade?',
+  image: cloudIcon,
+  body: '',
+  author: '',
+  time: ''
+}]
+
 class Posts extends Component {
   componentDidMount () {
   }
 
   render () {
+    const blogPosts = blogs.map((post) =>
+      <Post post={post} />
+    )
     return (
       <div style={{width: '100%'}}>
         <Slider style={{height: '100%'}} {...settings}>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          {blogPosts}
         </Slider>
       </div>
     )
